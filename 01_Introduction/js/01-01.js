5ebaba49af633c5935fd509dd3d9b88059b10f53
Dereje Kitaw <dereje.kitaw@gmail.com>
2019-12-01 00:23:26 -0500

    refactor

--- 01_Introduction/js/01-01.js
+++ 01_Introduction/js/01-01.js
@@ -2,7 +2,27 @@ function init() {
   console.log('This Three.js version is V-' + THREE.REVISION);
   // create a scene, that will hold all our elements such as objects, cameras and lights.
   const scene = new THREE.Scene();
+  // create a render and set the size
+  renderer = addRenderer();
+  const camera = addCamera(scene);
+  addAxis(scene);
+  addPlane(scene);
+  addCube(scene);
+  addSphere(scene);
+  addTree(scene);
+  // add the output of the renderer to the html element
+  document.getElementById('webgl-output').appendChild(renderer.domElement);
 
+  // render the scene
+  renderer.render(scene, camera);
+}
+function addRenderer() {
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor(new THREE.Color(0x000000));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  return renderer;
+}
+function addCamera(scene) {
   // create a camera, which defines where we're looking at.
   const camera = new THREE.PerspectiveCamera(
     45,
@@ -13,22 +33,7 @@ function init() {
   // position and point the camera to the center of the scene
   camera.position.set(-30, 40, 30);
   camera.lookAt(scene.position);
-  
-  // create a render and set the size
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor(new THREE.Color(0x000000));
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  addAxis(scene);
-  addPlane(scene);
-  addCube(scene);
-  addSphere(scene);
-
-  // add the output of the renderer to the html element
-  document.getElementById('webgl-output').appendChild(renderer.domElement);
-
-  // render the scene
-  renderer.render(scene, camera);
+  return camera;
 }
 function addAxis(scene) {
   // show axes in the screen
@@ -82,4 +87,34 @@ function addSphere(scene) {
   sphere.castShadow = true;
   // add the sphere to the scene
   scene.add(sphere);
+}
+function addTree(scene) {
+  var trunk = new THREE.CubeGeometry(1, 8, 1);
+  var leaves = new THREE.SphereGeometry(4);
+
+  // create the mesh
+  var trunkMesh = new THREE.Mesh(
+    trunk,
+    new THREE.MeshLambertMaterial({
+      color: 0x8b4513
+    })
+  );
+  var leavesMesh = new THREE.Mesh(
+    leaves,
+    new THREE.MeshLambertMaterial({
+      color: 0x00ff00
+    })
+  );
+
+  // position the trunk. Set y to half of height of trunk
+  trunkMesh.position.set(-10, 4, 0);
+  leavesMesh.position.set(-10, 12, 0);
+
+  trunkMesh.castShadow = true;
+  trunkMesh.receiveShadow = true;
+  leavesMesh.castShadow = true;
+  leavesMesh.receiveShadow = true;
+
+  scene.add(trunkMesh);
+  scene.add(leavesMesh);
 }
